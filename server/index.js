490c8e78d2141e608602fe892abd6bdeedeefd6a
ParthDevaliya522 <parthdevaliya1@gmail.com>
2026-08-09 17:57:07 +0530

    "Add live origin"

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://spendiqai.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
